ed871023f0a68cb68b53a5cf31171b12549033cf
James Ford <psyked84@gmail.com>
2018-07-12 08:54:41 +0100

    Add some supporting project / publishing / formatting files

---
+++ scripts/postversion.js
@@ -0,0 +1,13 @@
+const sh = require('shelljs');
+
+const { version } = require('../package.json');
+
+sh.exec(`git commit package.json package-lock.json CHANGELOG.md -m "Version ${version}"`);
+sh.echo();
+
+sh.echo(`git tag v${version}`);
+sh.exec(`git tag v${version}`);
+sh.echo();
+
+sh.exec('git push');
+sh.exec('git push --tags');
---
+++ scripts/preversion.js
@@ -0,0 +1,31 @@
+const sh = require('shelljs');
+
+const { version } = require('../package.json');
+
+sh.echo('Running unit tests...');
+
+const results = sh.exec('npm run test');
+
+if (results.code !== 0) {
+    sh.echo('ERROR: Please fix the failing tests before versioning.');
+    sh.exit(1);
+}
+
+sh.echo('Checking for an up-to-date CHANGELOG.md before versioning...');
+sh.echo();
+sh.echo('git status --porcelain');
+sh.echo();
+
+const status = sh.exec('git status --porcelain').stdout.trim();
+
+sh.echo();
+
+if (status.indexOf('M CHANGELOG.md') === -1) {
+    sh.echo('ERROR: Please update the CHANGELOG.md file before versioning.');
+    sh.exit(1);
+}
+
+if (status !== 'M CHANGELOG.md') {
+    sh.echo('ERROR: Please commit or stash these changes (excluding CHANGELOG.md).');
+    sh.exit(1);
+}
